8f4b299d47e01bf60e5405d21e1c3da4fe89bb4c
lpturmel <louisphilippe.turmel@gmail.com>
2026-04-13 18:19:17 -0400

    Added question 1

--- Question 1/Question_1.cs
+++ Question 1/Question_1.cs
@@ -1,17 +1,41 @@
 using System;
 using System.Collections.Generic;
 
+/* NOTE:
+ *
+ * Because the tax rule is the same for all items, I implemented 
+ * GetFullPrice() on the base class instead of forcing subclasses
+ * to override it. Item remains abstract because it 
+ * represents a base type that should not be instantiated.
+ *
+ * Since the question asks that each item must have a price and weight
+ * and repeating the same initialization code for each Item felt unnecessary,
+ * I made a base constructor on Item.
+ */
+
 public class Program
 {
     public static void Main()
     {
-        List<Item> items = new List<Item>();
-	
+
         //items contains all the items to buy
+        List<Item> items = new List<Item>();
+
         //add the table, paddle and balls with the required quantities
-        //Display the price of each item in items
-	
-        Console.ReadLine();
+        items.Add(new Table(100, 1));
+
+        items.Add(new Paddle(50, 0.5f));
+        items.Add(new Paddle(50, 0.5f));
+
+        items.Add(new Balls(20, 0.5f));
+        items.Add(new Balls(20, 0.5f));
+        items.Add(new Balls(20, 0.5f));
+
+        //Display the name of each item in items
+        foreach (Item item in items)
+        {
+            Console.WriteLine("{0}\t:\t${1:F2}", item.GetType().Name, item.GetFullPrice());
+        }
     }
 }
 
@@ -19,6 +43,32 @@ public abstract class Item
 {
     protected int price;
     protected float weight;
-    abstract public float getFullPrice();
-    public float Weight { get {return weight;} }
+
+    protected Item(int price, float weight)
+    {
+        this.price = price;
+        this.weight = weight;
+    }
+
+    public float GetFullPrice()
+    {
+        return price * 1.2f;
+    }
+
+    public float Weight { get { return weight; } }
+}
+
+public class Table : Item
+{
+    public Table(int price, float weight) : base(price, weight) { }
+}
+
+public class Paddle : Item
+{
+    public Paddle(int price, float weight) : base(price, weight) { }
+}
+
+public class Balls : Item
+{
+    public Balls(int price, float weight) : base(price, weight) { }
 }
